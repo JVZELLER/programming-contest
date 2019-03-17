f67cb7673f9c89869b73066cb32893bc2b1313a1
Otávio Augusto <otavio.porto@fatec.sp.gov.br>
2019-03-16 22:09:33 -0300

    Solução 1161

---
+++ problems/C++/1161_Soma_de_fatoriais/1161_Soma_de_fatoriais.cpp
@@ -0,0 +1,30 @@
+/*
+	author: Otávio Augusto - OAMP
+	problem_name: Soma de Fatoriais
+	problem_number: 1161
+	category: Matemática
+	difficulty_level: 6
+	link: https://www.urionlinejudge.com.br/judge/pt/problems/view/1161
+*/
+
+#include <iostream>  // entrada e saída padrão
+
+using namespace std;
+using ull = unsigned long long;  // novo nome p/ unsigned long long
+
+// função recusiva de cauda, valor passado p/ referência
+inline void fact(ull& val, int mult) { 
+    if (val < 2) val = 1;  // menor que 2? retorna 1
+    if (mult > 1) fact(val *= mult, mult - 1);  // chama func. com novos valores
+}  // fim da função
+
+int main() {
+    ull m, n;  // variáveis p/ cálculo
+    
+    while (cin >> m >> n) {  // entrada até EOF
+        fact(m, m - 1);  // chamada da função
+        fact(n, n - 1);  // chamada da função
+        cout << m + n << endl;  // cout da soma
+    }  // fim enquanto
+    return 0;  // fim do programa
+}
