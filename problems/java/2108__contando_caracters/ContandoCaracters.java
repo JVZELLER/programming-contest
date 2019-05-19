32c4931e0dba9c645215668d04a992a45b2b41c2
José Victor Zeller <josevictorzg@gmail.com>
2019-05-19 19:18:21 -0300

    add: solve uri 2108 problem

---
+++ problems/java/2108__contando_caracters/ContandoCaracters.java
@@ -0,0 +1,59 @@
+package com.maratona.problemas;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/**
+ * 
+ * @author zeller
+ *
+ * @problem_name Contando Caracters
+ * 
+ * @problem_number 2108
+ * 
+ * @category Strings
+ * 
+ * @difficulty_level 4.4
+ * 
+ * @see <a href=
+ *      "https://www.urionlinejudge.com.br/judge/pt/problems/view/2108">Link URI
+ *      Online Judge</a>
+ * 
+ */
+public class ContandoCaracters {
+
+	public static void main(String[] args) throws IOException {
+		int n, numeroBota, contador = 0;
+		String s, parBota;
+		int[] direita = new int[60];
+		int[] esquerda = new int[60];
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+		while ((s = in.readLine()) != null) {
+			n = Integer.parseInt(s);
+			Arrays.fill(esquerda, 0);
+			Arrays.fill(direita, 0);
+			contador = 0;
+			while (n-- > 0) {
+				String[] line = in.readLine().split(" ");
+				numeroBota = Integer.parseInt(line[0]);
+				parBota = line[1];
+				if (parBota.equals("D")) {
+					direita[numeroBota - 1] += 1;
+				} else {
+					esquerda[numeroBota - 1] += 1;
+				}
+			}
+
+			for (int i = 0; i < esquerda.length; i++) {
+				int j = esquerda[i] > direita[i] ? direita[i] : esquerda[i];
+				contador += j;
+			}
+
+			System.out.println(contador);
+		}
+	}
+
+}
